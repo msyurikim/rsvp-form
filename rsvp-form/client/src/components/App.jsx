0305e406ff891ba6b2aa81f0024f8c206490affd
Yuri Kim <yurikim123@gmail.com>
2020-06-01 07:33:39 -1000

    completed front end

--- rsvp-form/client/src/components/App.jsx
+++ rsvp-form/client/src/components/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import $ from 'jquery';
 
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
 
   }
 
-  render() {
+  addToRSVP(attendee) {
+    $.post('/rsvps', attendee)
+      .done((data) => {
+        console.log('Data loaded: ', data);
+      })
+      .fail((err) => {
+        console.log(err);
+      });
+  }
 
+  render() {
+    return (
+      <Form addToRSVP={this.addToRSVP}/>
+    );
   }
 }
 
